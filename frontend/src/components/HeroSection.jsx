c65db9b274e29067df3b7560ee24dc2a6935a89e
Narayan Dwivedi <desinplus@gmail.com>
2026-07-21 20:43:46 +0530

    improve hero

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -2,13 +2,13 @@ import heroImg from '../hero (3).png';
 
 export default function HeroSection() {
   return (
-    <section className="relative w-full min-h-[75vh] bg-[#FFF8F0] flex items-center justify-center overflow-hidden">
+    <section className="relative w-full min-h-fit md:min-h-[75vh] bg-[#FFF8F0] overflow-hidden">
       <img
         src={heroImg}
         alt=""
-        className="absolute inset-0 w-full h-full object-contain"
+        className="w-full md:absolute md:inset-0 md:w-full md:h-full md:object-contain md:object-top"
       />
-      <div className="relative z-10 w-full h-full" />
+      <div className="hidden md:block relative z-10 w-full h-full" />
     </section>
   );
 }
